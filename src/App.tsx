a612de97dc84f78b49db42ce8f856406f126ce90
cassiocsantana <cassiocsantana@hotmail.com>
2020-04-30 13:04:56 -0400

    Criando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
+import Routes from './Router';
 
-function App() {
-  return (
-    <h1>Hello World</h1>
-  );
-}
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>
+);
 
 export default App;
